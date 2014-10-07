c4c876faf069d1f545823e11b574e4845b16e5bb
Brittany Robertson <brittanyrob@live.ca>
2014-10-07 08:47:38 -0400

    sun

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var $win = $(window);
 var $sun = $('.sun');
+var $sunSection =$('.sun-section');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 	
 	/*console.log(scrollPos);  For debugging*/
-	$sun.css('transform', 'rotate('  + scrollPos + ' deg)');
+	$sun.css('transform', 'rotate('  + scrollPos / 5 + 'deg)');
+	$sunSection.css('background-position', '');
 });
